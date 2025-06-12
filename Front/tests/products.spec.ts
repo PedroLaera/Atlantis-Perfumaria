e94f2014d19c7def4d1ec1fdb1729ffb12d379bc
Vistorgiese <Vitorgiese@hotmail.com>
2025-06-12 14:47:03 -0300

    alterando test product

--- Front/tests/products.spec.ts
+++ Front/tests/products.spec.ts
@@ -1,34 +1,43 @@
-import { test, expect } from '@playwright/test';
-
+import { test, expect } from "@playwright/test";
 
 // --- Dados do Usuário em Constantes ---
 //const NOME_USUARIO = 'Pedro Laera';
-const EMAIL_USUARIO = 'pedroLaele@hotmail.com';
-const SENHA_USUARIO = '12345678@';
+const EMAIL_USUARIO = "pedroLaele@hotmail.com";
+const SENHA_USUARIO = "12345678@";
 //const CPF_USUARIO = '275.538.590-10';
 
-test('Crud Produto', async ({ page }) => {
-  await page.goto('http://localhost/login'); 
+test("Crud Produto", async ({ page }) => {
+  await page.goto("http://localhost/login");
+
+  await page.locator('input[name="email"]').fill(EMAIL_USUARIO);
+  await page.locator('input[name="password"]').fill(SENHA_USUARIO);
+  await page.getByRole("button", { name: /entrar/i }).click();
+
+  await expect(page).toHaveURL("http://localhost/profile");
+
+  await page.getByRole("link", { name: /Adicionar Produto/i }).click();
+
+  await page.getByRole("button", { name: /Categorias/i }).click();
+
+  await expect(page).toHaveURL("http://localhost/addproduct");
+
+  await page.getByRole("link", { name: /Adicionar Nova Categoria/i }).click();
+
+  await page.locator('input[name="name"]').fill("test");
+
+  await page.getByRole("button", { name: /Cadastrar/i }).click();
+
+  await page.getByRole("button", { name: /Categorias/i }).click();
+
+  const todasAsLinhas = page.getByRole('row');
+
+  const linhaEspecifica = todasAsLinhas.filter({hasText: test })
+
   
-    await page.locator('input[name="email"]').fill(EMAIL_USUARIO);
-    await page.locator('input[name="password"]').fill(SENHA_USUARIO);
-    await page.getByRole('button', { name: /entrar/i }).click();
-  
-    await expect(page).toHaveURL('http://localhost/profile');
 
-    await page.getByRole('link', { name: /Adicionar Produto/i }).click();
+  await page.getByRole("link", { name: /editar/i }).click();
 
-    await page.getByRole('button', { name: /Categorias/i }).click();
+  await page.locator('input[name="name"]').fill("test edited");
 
-    await expect(page).toHaveURL('http://localhost/addproduct');
-    
-    await page.getByRole('link', { name: /Adicionar Nova Categoria/i }).click();
-
-    await page.locator('input[name="name"]').fill('teste'); 
-
-    await page.getByRole('button', { name: /Cadastrar/i }).click();
-
-
+  await page.getByRole("button", { name: /Salvar Alterações/i }).click();
 });
-
-
